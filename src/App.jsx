ad3e30a826b5e6f13a8c1bbff44ff3e1b9077905
cristian <cristian192003@outlook.es>
2022-11-23 09:54:09 -0600

    Incio del proyecto: se definio el css y se empezo con la cabecera

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Cabecera from "./Cabecera/Cabecera";
+function App() {
+  return (
+    <div>
+      <Cabecera/>
+      <h2>App</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cabecera/Cabecera.jsx
@@ -0,0 +1,9 @@
+function Cabecera(){
+    return(
+        <div>
+            <h1>Cabecera</h1>
+        </div>
+    );  
+}
+
+export default Cabecera;
