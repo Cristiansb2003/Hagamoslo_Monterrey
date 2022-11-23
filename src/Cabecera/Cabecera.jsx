fc359397c54a3e374d274d8ee914edccdac4a315
cristian <cristian192003@outlook.es>
2022-11-23 11:40:00 -0600

    Se agrego el carrucel con la opacidad requerida

--- src/Cabecera/Cabecera.jsx
+++ src/Cabecera/Cabecera.jsx
@@ -1,9 +1,30 @@
-function Cabecera(){
-    return(
-        <div>
-            <h1>Cabecera</h1>
+import Menu from "../Menu/Menu";
+import img1 from "./../assets/actividad_piloto/1.jpg";
+import st from "./cabecera.module.css";
+function Cabecera() {
+  return (
+    <div className={st.contenedor}>
+      {/* <Menu/> */}
+      <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={img1} className="d-block w-100" alt="..." />
+          </div>
+
+          <div className="carousel-item">
+            <img src={img1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={img1} className="d-block w-100" alt="..." />
+          </div>
         </div>
-    );  
+      </div>
+    </div>
+  );
 }
 
 export default Cabecera;
---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,9 @@
+
+import st from './menu.module.css'
+function Menu(){
+    return (
+        <div  className={st.contenedor}>Menu</div>
+    )
+}
+
+export default Menu;
