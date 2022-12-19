3b3210cf0c79e327f72e060201451faf2157affc
cristian <cristian192003@outlook.es>
2022-12-19 12:19:32 -0600

    Se agrego el pie de pagina

---
+++ src/components/piePagina/piePagina.jsx
@@ -0,0 +1,9 @@
+function PiePagina(){
+    return(
+        <div>
+            <h3>Pie de Pagina</h3>
+        </div>
+    )
+}
+
+export default PiePagina
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Cabecera from "../../components/cabecera/Cabecera";
 import CarAct from "../../components/carrAct/CarAct";
 import Mision from "../../components/mision/Mision";
+import PiePagina from "../../components/piePagina/piePagina";
 import Vision from "../../components/vision/Vision";
 
 function Home() {
@@ -10,6 +11,8 @@ function Home() {
       <Mision />
       <Vision />
       <CarAct />
+      <PiePagina/>
+      <PiePagina/><PiePagina/>
     </div>
   );
 }
