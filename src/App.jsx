8f7f650ed2d5b04be76d2504cfeb7ebd61b67076
cristian <cristian192003@outlook.es>
2022-12-29 17:43:25 -0600

    Se cambio la dimension de las imagenes a 1080px

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import "./App.css";
 import CardAct from "./components/cardAct/CardAct";
 import GalActividades from "./pages/galActividades/galActividades";
 import GalImg from "./pages/galImg/GalImg";
+import act_2022 from "./assets/img/act-2022/act-2022";
+import Imagenes from "./assets/img/act-2022/act-piloto/images";
 function App() {
   return (
     <div>
-      <GalImg/>
+      {/* {
+        Imagenes.map((ima)=>(
+          <img src={ima.img} key ={ima.pos}></img>
+        ))
+      } */}
       {/* <GalActividades/> */}
+      <GalImg/>
+      {/* <div className="contenedor">
+        <div className="padre">
+          <div className="imagen"></div>
+        </div>
+      </div> */}
     </div>
   );
 }
