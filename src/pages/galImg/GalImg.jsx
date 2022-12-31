1014e7e3b9cf5409469b3772cc6daa4947218812
cristian <cristian192003@outlook.es>
2022-12-30 23:22:19 -0600

    prueba

--- src/pages/galImg/GalImg.jsx
+++ src/pages/galImg/GalImg.jsx
@@ -1,17 +1,37 @@
-import st from "./galImg.module.css"
+import st from "./galImg.module.css";
+import "./style.css"
 import Imagenes from "../../assets/img/act-2022/act-piloto/images";
+import { useState } from "react";
 // Esta es una pagina para listar todas las actividades
 function GalImg() {
+  const [model, setModel] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+    console.log(imgSrc)
+  };
   return (
-    <div className={st.contenedor}>
-      <div className={st.col}>
-        {Imagenes.map((img, index) => (
-          <div key={index} className={st.cuadro}>
-            <img src={img.img} />
-          </div>
-        ))}
+    <>
+      <div className={model ? "model open" : "model"}>
+        <div className="con">
+        <img src={tempImgSrc} />
+        </div>
       </div>
-    </div>
+      <div className={st.contenedor}>
+        <div className={st.col}>
+          {Imagenes.map((img, index) => (
+            <div
+              key={index}
+              onClick={() => getImg(img.img)}
+              className={st.cuadro}
+            >
+              <img src={img.img} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 export default GalImg;
